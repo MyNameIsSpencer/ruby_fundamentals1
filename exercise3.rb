37ead3979a579cda30a464fd519db97fc9c3d8a5
spencer kyonka <spencer.kyonka@hotmail.ca>
2017-10-30 16:59:00 -0400

    done part 1 of exercise 3

--- exercise3.rb
+++ exercise3.rb
@@ -1,5 +1,16 @@
+# What is your name and age?
+
 puts "What is your name?"
 
-user_name = gets
+user_name = gets.chomp
 
-puts "Hello #{user_name}" + "I hope you are happy today and tip me well."
+puts "Hello #{user_name}" + ", I hope you are happy today and tip me well."
+
+puts "How old are you #{user_name}?"
+
+user_age = gets.chomp
+
+
+puts "WOW! You only look #{user_age.to_i - 7} years old!!"
+
+# if portion
