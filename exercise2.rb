4adfe9421e089ab1524571bb23b12d278a725f57
spencer kyonka <spencer.kyonka@hotmail.ca>
2017-10-30 16:09:33 -0400

    done exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,30 @@
+#Question 1.
+
+meal = 55.00
+
+tip_rate = 0.20
+
+
+tip = meal * tip_rate
+
+total = meal + tip
+
+puts " The tip is $#{tip} and The total is : $#{total}"
+
+#Question 2
+
+abbel = 8
+
+puts "twenty " + abbel.to_s
+
+
+#Question 3
+
+puts "45628 multiplied by 7839 is #{45628 * 7839}"
+
+
+#Question 4
+
+puts "I guess the output of said expression will be true"
+
+puts "#{(10 < 20 && 30 < 20) || !(10 == 11)}"
